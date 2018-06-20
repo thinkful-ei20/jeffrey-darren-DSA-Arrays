f46ad424edb468838428a53dbde77d8e40a145da
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-20 11:01:40 -0500

    Updated with SIZE_RATIO

--- Array.js
+++ Array.js
@@ -5,11 +5,15 @@ const memory = require('./memory');
 class Array {
   constructor(){
     this.length = 0;
+    this._capacity = 0;
     this.ptr = memory.allocate(this.length);
   }
 
   push(value){
-    this._resize(this.length + 1);
+    if(this.length >= this._capacity) {
+      this._resize((this.length+ 1) * Array.SIZE_RATIO);
+    }
+    
     memory.set(this.ptr + this.length, value);
     this.length++;
   }
@@ -17,21 +21,13 @@ class Array {
   _resize(size){
     const oldPtr = this.ptr;
     this.ptr = memory.allocate(size);
-
+    
     if(this.ptr === null){
       throw new Error('Out of memory');
-    }
+    }    
     memory.copy(this.ptr, oldPtr, this.length);
     memory.free(oldPtr);
+    this._capacity = size;
   }
-
-  
-
-
-
-
-
-
-
-
 }
+Array.SIZE_RATIO = 3;
