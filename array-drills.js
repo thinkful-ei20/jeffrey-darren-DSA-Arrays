27f6938b80fb489a8966b12afee3fd6643cbf87a
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-20 13:42:02 -0500

    Max sum in the array

--- array-drills.js
+++ array-drills.js
@@ -10,3 +10,30 @@ output = urlify('tauhida parveen');
 console.log(output);
 output = urlify('www.thinkful.com /tauh ida parv een');
 console.log(output);
+// O(n)
+
+
+/*
+Max sum in the array
+
+*/
+
+function maxSumArr(arr) {
+  let max = 0;
+  while (arr.length > 0) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    if (result > max) max = result;
+    
+    arr.pop();
+  }
+
+  return max;
+}
+
+output = maxSumArr([4, 6, -3, 5, -2, 1]);
+console.log(output);
+// O(n)
+
